80476cb5e74367e157cc21a1b018a8f66c6e69c8
Abdulaziz <aboal3z2013@gmail.com>
2021-12-08 01:53:29 +0300

    fix the typing bug and add timer

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,67 @@
 import { useState, useEffect } from 'react';
 import textdata from '../LocalData/data';
 export default function Home() {
-	const [data, setData] = useState('');
-	const [textValue, setTextValue] = useState();
+	const [data, setData] = useState('Hello');
 	const [charPointer, setCharPointer] = useState(0);
 	const [rightPointer, setRightPointer] = useState(0);
-	const [wrongFlag, setWrongFlag] = useState(false);
+	const [time, setTime] = useState(0);
+	const [start, setStart] = useState(false);
+	const [startText, setStartText] = useState(false);
 
-	var randomProperty = function () {
-		var keys = Object.keys(textdata);
-		setData(textdata[keys[(keys.length * Math.random()) << 0]]);
-	};
+	// var randomProperty = function () {
+	// 	var keys = Object.keys(textdata);
+	// 	setData(textdata[keys[(keys.length * Math.random()) << 0]]);
+	// };
 
+	// useEffect(() => {
+	// 	// randomProperty(textdata);
+	// }, []);
 	useEffect(() => {
-		randomProperty(textdata);
-	}, []);
+		let interval = null;
+
+		if (start) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime + 10);
+			}, 10);
+		} else {
+			clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+	}, [start]);
 
 	const onType = (e) => {
+		if (startText == false) {
+			setStartText(true);
+			setStart(true);
+		}
+		if (
+			data.length === data.slice(0, rightPointer + 1).length &&
+			start == true
+		) {
+			setStartText(false);
+			setStart(false);
+		}
 		setCharPointer(e.target.value.length);
 		if (data.slice(0, charPointer + 1) === e.target.value) {
 			setRightPointer(e.target.value.length);
 		}
-		setTextValue(e.target.value);
-		console.log(data.slice(0, charPointer + 1), e.target.value);
 	};
+
+	const onDelete = (e) => {
+		if (e.key === 'Backspace' && e.target.value.length !== 0) {
+			setCharPointer(e.target.value.length - 1);
+
+			if (charPointer == rightPointer) {
+				setRightPointer(rightPointer - 1);
+			}
+		}
+	};
+	// console.log('CP: ' + charPointer, 'RP: ' + rightPointer);
+	// console.log(
+	// 	'CP: ' + data.slice(0, charPointer),
+	// 	'RP: ' + data.slice(0, rightPointer)
+	// );
+	// console.log(start);
 
 	return (
 		<div className="flex flex-col items-center h-screen py-2 bg-gray-100">
@@ -32,6 +70,10 @@ export default function Home() {
 					Speed Type
 				</span>
 				<div className="flex flex-col justify-center items-center max-w-screen-lg h-full space-y-20">
+					<div className="text-left text-2xl font-bold text-gray-400 font-inter">
+						<span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+						<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+					</div>
 					<span className="text-left text-2xl font-bold text-gray-600 font-inter">
 						{data.split('').map((i, x) => {
 							if (x < charPointer) {
@@ -58,9 +100,13 @@ export default function Home() {
 						<input
 							placeholder="Go"
 							onChange={onType}
+							ref={(input) => input && input.focus()}
+							onKeyDown={onDelete}
 							className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200 w-full"
 						/>
 					</div>
+					{/* <button onClick={() => setStart(true)}>Start</button>
+					<button onClick={() => setStart(false)}>Stop</button> */}
 				</div>
 			</main>
 		</div>
