2a6233a279119d37c45f99a200f5743dcc88108c
Abdulaziz <aboal3z2013@gmail.com>
2021-12-08 04:37:52 +0300

    add start page and result page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
 	const [start, setStart] = useState(false);
 	const [startText, setStartText] = useState(false);
 	const [Wpm, setWpm] = useState(0);
+	const [step, setStep] = useState(0);
 
 	var randomProperty = function () {
 		var keys = Object.keys(textdata);
@@ -16,7 +17,7 @@ export default function Home() {
 
 	useEffect(() => {
 		randomProperty(textdata);
-	}, []);
+	}, [data]);
 	useEffect(() => {
 		let interval = null;
 
@@ -42,6 +43,7 @@ export default function Home() {
 			setStartText(false);
 			setStart(false);
 			WPM();
+			setStep(3);
 		}
 		setCharPointer(e.target.value.length);
 		if (data.slice(0, charPointer + 1) === e.target.value) {
@@ -67,12 +69,6 @@ export default function Home() {
 		wpm = wpm / now;
 		setWpm(Math.round(wpm));
 	};
-	// console.log('CP: ' + charPointer, 'RP: ' + rightPointer);
-	// console.log(
-	// 	'CP: ' + data.slice(0, charPointer),
-	// 	'RP: ' + data.slice(0, rightPointer)
-	// );
-	// console.log(start);
 
 	return (
 		<div className="flex flex-col items-center h-screen py-2 bg-gray-100">
@@ -80,43 +76,99 @@ export default function Home() {
 				<span className="p-4 text-6xl font-bold font-houseslant text-gray-700">
 					Speed Type
 				</span>
-				<div className="flex flex-col justify-center items-center max-w-screen-lg h-full space-y-20">
-					<div className="text-left text-2xl font-bold text-gray-400 font-inter">
-						<span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-						<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span>
-					</div>
-					<span className="text-left text-2xl font-bold text-gray-600 font-inter">
-						{data.split('').map((i, x) => {
-							if (x < charPointer) {
-								if (x < rightPointer) {
-									return (
-										<span className="text-green-400 bg-gray-200 border-b-2 border-green-400">
-											{i}
-										</span>
-									);
-								} else {
-									return (
-										<span className="text-red-400 bg-gray-200 border-b-2 border-red-400">
-											{i}
-										</span>
-									);
-								}
-							} else {
-								return <span>{i}</span>;
-							}
-						})}
-					</span>
+				<div className="flex flex-col justify-center items-center max-w-screen-lg h-full space-y-20 p-x-4">
+					{step == 0 ? (
+						<>
+							<span className="text-left text-2xl font-bold text-gray-600 font-inter">
+								Start Tisting you’r self and improve you’r typing skils.
+							</span>
+							<button
+								className="block uppercase mx-auto shadow bg-red-400  focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded font-inter"
+								onClick={() => setStep(1)}
+							>
+								Start
+							</button>
+						</>
+					) : step == 1 ? (
+						<>
+							{/* <div className="text-left text-2xl font-bold text-gray-400 font-inter"> */}
+							{/* <span>
+									{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
+								</span>
+								<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+							</div> */}
+							<span className="text-left text-2xl font-bold text-gray-600 font-inter">
+								{data.split('').map((i, x) => {
+									if (x < charPointer) {
+										if (x < rightPointer) {
+											return (
+												<span className="text-gr een-400 bg-gray-200 border-b-2 border-green-400">
+													{i}
+												</span>
+											);
+										} else {
+											return (
+												<span className="text-red-400 bg-gray-200 border-b-2 border-red-400">
+													{i}
+												</span>
+											);
+										}
+									} else {
+										return <span>{i}</span>;
+									}
+								})}
+							</span>
 
-					<div className="w-full ">
-						<input
-							placeholder="Go"
-							onChange={onType}
-							ref={(input) => input && input.focus()}
-							onKeyDown={onDelete}
-							className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200 w-full"
-						/>
-					</div>
-					<div>{Wpm}</div>
+							<div className="w-full ">
+								<input
+									placeholder="Go"
+									onChange={onType}
+									ref={(input) => input && input.focus()}
+									onKeyDown={onDelete}
+									className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200 w-full"
+								/>
+							</div>
+						</>
+					) : (
+						<div className="flex flex-col space-y-20">
+							<div className="flex flex-row space-x-20">
+								<span className="text-left text-2xl font-bold text-gray-600 font-inter">
+									You'r WPM: {Wpm}
+								</span>
+								<span className="text-left text-2xl font-bold text-gray-600 font-inter">
+									Time: {Math.floor((time / 60000) % 60)}:
+									{Math.floor((time / 1000) % 60)}
+								</span>
+							</div>
+							<div className="flex flex-row  mx-auto">
+								<button
+									className="block uppercase  shadow bg-red-400 font-inter  focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded-tl rounded-bl font-bold"
+									onClick={() => {
+										setTime(0);
+										setWpm(0);
+										setCharPointer(0);
+										setRightPointer(0);
+										setStep(1);
+									}}
+								>
+									Play Again
+								</button>
+								<button
+									className="block uppercase  shadow bg-green-400 font-inter  focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded-tr rounded-br font-bold"
+									onClick={() => {
+										setTime(0);
+										setWpm(0);
+										setCharPointer(0);
+										setRightPointer(0);
+										setStep(1);
+										setData('');
+									}}
+								>
+									Play Again
+								</button>
+							</div>
+						</div>
+					)}
 				</div>
 			</main>
 		</div>
